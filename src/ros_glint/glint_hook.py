9ae81589f8f67056243da3344f5a757c2c6452ed
David V. Lu <davidvlu@gmail.com>
2024-02-24 22:15:06 -0500

    Minor cleanup

--- src/ros_glint/glint_hook.py
+++ src/ros_glint/glint_hook.py
@@ -5,17 +5,8 @@ from ros_introspect import find_packages
 
 
 def main():
-    linters = get_linters()
-
-    class ValidateCleaner(argparse.Action):
-        def __call__(self, parser, args, values, option_string=None):
-            for value in values:
-                if value not in linters:
-                    raise ValueError(f'{value} not a valid linter!')
-            args.linters = values
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('linters', nargs='*', action=ValidateCleaner, metavar='linter', default=[])
+    parser.add_argument('linters', nargs='*', metavar='linter', default=[])
     parser.add_argument('-f', '--related_files', type=pathlib.Path, nargs='*')
 
     args = parser.parse_args()
@@ -29,7 +20,7 @@ def main():
     ret = 0
 
     for _, package in sorted(pkgs.items()):
-        for name, fne in linters.items():
+        for name, fne in get_linters().items():
             if args.linters and name not in args.linters:
                 continue
             fne(package)
