30a0b615b9764971d6a368a26288d3d600e08776
David V. Lu <davidvlu@gmail.com>
2024-02-26 13:59:20 -0500

    Groups

--- sync_hooks.py
+++ sync_hooks.py
@@ -9,29 +9,46 @@ REPLACE_WORDS = {
     'Cmake': 'CMake',
     'Cpp': 'C++',
     'Cplusplus': 'C++',
-    'Misc': 'Miscelaneous',
+    'Misc': 'Miscellaneous',
     'Rviz': 'RViz',
     'Xml': 'XML',
 }
+GROUPS = {
+    'prettify_cmake': ['prettify_catkin_package_cmd',
+                       'prettify_package_lists',
+                       'prettify_msgs_srvs',
+                       'prettify_installs',
+                       'prettify_command_groups'],
+    'remove_boilerplate': ['remove_empty_export_tag',
+                           'remove_boilerplate_manifest_comments',
+                           'remove_boilerplate_cmake_comments'],
+}
 
 
-def get_words(s):
+def to_english(s):
     words = []
     for word in identifier_split(s):
         word = word.title()
         word = REPLACE_WORDS.get(word, word)
         words.append(word)
 
-    return words
+    return ' '.join(words)
 
 
 data = []
-for function_id, fne in get_linters().items():
+for function_id in get_linters().keys():
     row = {'id': function_id}
-    row['name'] = ' '.join(get_words(function_id))
+    row['name'] = to_english(function_id)
     row['entry'] = f'glint_hook {function_id} -f'
     row['language'] = 'python'
     # TODO: Limit to certain files
+    data.append(row)
+
+for group_name, functions in GROUPS.items():
+    row = {'id': group_name}
+    row['name'] = to_english(group_name)
+    row['entry'] = f'glint_hook {" ".join(functions)} -f'
+    row['language'] = 'python'
     data.append(row)
 
 yaml.dump(data, open(path, 'w'))
